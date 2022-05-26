53d43f4e2cb8bec4fb37603f926a093684abf10c
Keneth Riera <kenethrr@outlook.com>
2022-05-25 23:39:13 -0500

    Use of "if let" and "while let" added

--- 11. closures/src/main.rs
+++ 11. closures/src/main.rs
@@ -16,4 +16,44 @@ fn main() {
     add();
     println!("{}", counter);
     println!("{}", variable);
+
+    let age: Option<u32> = Some(20);
+
+    if let Some(age) = age {
+        println!("Age -> {}", age);
+    }
+
+    let mut messages: Option<i32> = Some(50);
+
+    println!("Mensajes: {}", messages.unwrap_or_default());
+    // poco legible
+    loop {
+        match messages {
+            Some(value) => {
+                if value > 0 {
+                    println!("Tienes {} mensajes en tu bandeja.", value);
+                    messages = Some(value - 1);
+                } else {
+                    println!("No tienes mensajes en tu bandeja.");
+                    messages = None;
+                }
+            },
+            None => { break; }
+        }
+    }
+    println!("Mensajes: {}", messages.unwrap_or_default());
+
+    // más legible
+    messages = Some(50);
+    println!("Mensajes: {}", messages.unwrap_or_default());
+    while let Some(value) = messages {
+        if value > 0 {
+            println!("Tienes {} mensajes en tu bandeja.", value);
+            messages = Some(value - 1);
+        } else {
+            println!("No tienes mensajes en tu bandeja.");
+            messages = None;
+        }
+    }
+    println!("Mensajes: {}", messages.unwrap_or_default());
 }
